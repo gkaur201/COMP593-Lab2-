8f39b73aa38f6a539e75fd13b760d84155dbeea3
gkaur201 <156548687+gkaur201@users.noreply.github.com>
2024-01-24 11:15:53 -0500

    completed step 3

--- lab2_script_template.py
+++ lab2_script_template.py
@@ -12,6 +12,10 @@ def main():
     }
 
     # TODO: Step 3 - Add another movie to the data structure
+    new_movie = {"tittle": "anchorman", "genre": "comedy"}
+    about_me["movies"].append(new_movie)
+
+    print_student_name_and_id(about_me)
     
 # TODO: Step 4 - Function that prints student name and ID	
 def print_student_name_and_id(about_me):
