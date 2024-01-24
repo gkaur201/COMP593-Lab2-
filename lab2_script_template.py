c751361ea369cdbc779367da7108a13a63cdbd50
gkaur201 <156548687+gkaur201@users.noreply.github.com>
2024-01-24 11:27:08 -0500

    Completed step 4
    Finished print student name and ID

--- lab2_script_template.py
+++ lab2_script_template.py
@@ -16,9 +16,15 @@ def main():
     about_me["movies"].append(new_movie)
 
     print_student_name_and_id(about_me)
-    
+
 # TODO: Step 4 - Function that prints student name and ID	
 def print_student_name_and_id(about_me):
+    full_name = about_me["full_name"]
+    first_name = full_name.split()[0]
+    student_id = about_me["student_id"]
+    print(
+        f"My name is {full_name}, but you can call me Miss {first_name}.\nMy student ID is {student_id}."
+    )
     return
     
 # TODO: Step 5 - Function that adds pizza toppings to data structure
